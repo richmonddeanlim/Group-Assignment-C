b4e4d44fa442a7a725afad2ce065586e4b065b80
Richmond Deanlim <richmonddeanlim14@gmail.com>
2025-05-31 00:56:02 +0800

    adding the choice_valdiation and make menu fucntion

--- function/inventory_record.c
+++ function/inventory_record.c
@@ -1,9 +1,89 @@
 #include <stdio.h>
+#include <ctype.h>
 #include "../header/decoration.h"
 #include "decoration.c"
 
+// data structure for the inventory_record
+struct inventory_record
+{
+    char product_id[100];
+    int quantity;
+    char status[50];
+};
+
+// option validation function
+void option_validation(char *input_text,int *choice,int option)
+{
+    while(1)
+    {
+        printf("%s", input_text); 
+        if(scanf("%d", choice))
+        {
+            for(int i = 0; i < option;i++)
+            {
+                if(*choice == i + 1)
+                {
+                    return;
+                }
+    
+                else if(*choice != i && i == option - 1)
+                {
+                    printf("There is no such option\n");
+                }
+            } 
+        }
+    
+        else
+        {   char ch;
+            printf("Pls input an integer\n");
+            while ((ch = getchar()) != '\n' && ch != EOF);
+        }
+    }
+}
+
+// int valdiation
+
+
+
+
+// inventory record management menu
+int menu()
+{
+   // giving menu interface for user
+    border(default_border);
+    add_space(15);
+    printf("Iventory and Stock management\n");
+    border(default_border);
+    printf("1. Add stock inventory record\n");
+    printf("2. Update stock level\n");
+    printf("3. Delete Discontinued Stock\n");
+    printf("4. View Current Invetory\n");
+    border(default_border);
+    
+    // geting user choices
+    int choice;
+    option_validation("Enter your choice: ",&choice,5);
+    printf("%d", choice);
+
+    return choice; 
+}
+
+// function adding new record for inventory
+void add_record()
+{
+    struct inventory_record record;
+
+    printf("");
+     
+    
+}
+
+
+
+
 int main()
 {
+
     // giving menu interface for user
     border(default_border);
     add_space(15);
@@ -15,5 +95,10 @@ int main()
     printf("4. View Current Invetory\n");
     border(default_border);
     
+    // geting user choices
+    int choice;
+    option_validation("Enter your choice: ",&choice,5);
+    printf("%d", choice);
+
     return 0;
 }   
