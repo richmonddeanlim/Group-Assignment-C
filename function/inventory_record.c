5fb608d7f5ca83c371c5e31d3f25b43d84aa0f6e
Richmond Deanlim <richmonddeanlim14@gmail.com>
2025-06-02 22:06:11 +0800

    finishing the input from user and the validation

--- function/inventory_record.c
+++ function/inventory_record.c
@@ -50,16 +50,18 @@ void option_validation(char *input_text,int *choice,int range)
 // int valdiation
 void integer_valdiation(char *input_text,int *user_input )
 {
+    char ch;
     while(1)
     {
         printf("%s", input_text); 
         if(scanf("%d", user_input))
         {
+            while ((ch = getchar()) != '\n' && ch != EOF);
             return;
         }
-    
+        
         else
-        {   char ch;
+        {
             printf("Pls input an integer\n");
             while ((ch = getchar()) != '\n' && ch != EOF);
         }
@@ -108,73 +110,134 @@ int add_record()
     // File location
     struct inventory_record record;
     char ch;
+    char condition;
 
-    // getting date input
-    while(1)
-    {
-        int array_len = 0;
-        
-        // getting user input
-        printf("Enter the date (dd-mm-yyyy): ");
-        scanf("%s", &record.date);
-        
-        // getting arr len
-        arr_len(record.date, &array_len);
-        
-        // further date validation
-        if(array_len == 10)
-        {
-            if(record.date[2] == '-' && record.date[5] == '-')
-            {
-                // ensure the date not above 31
-                if( (int) (((record.date[0] - '0') * 10) + record.date[1] - '0') < 31) // subtract with '0' will return the int value (converting ascii to int)
-                {
-                    // ensure the month not above 12
-                    if((int) (((record.date[3] - '0') * 10) + record.date[4] - '0') < 12)
-                    {
-                        while ((ch = getchar()) != '\n' && ch != EOF);// clearing previous input     
-                        break;
-                    }
+    // open the product.txt
+    file_product = fopen(product_location,"r");
     
+    if( file_product == NULL)
+    {
+        printf("Error opening file.\n");
+        return 1;
+    }
+    
+    int line = 0;
+    char raw_product [100] [100];
+    
+    // take all line to the array until not end of file and not error
+    while(!feof(file_product) && !ferror(file_product))
+    {
+        // looping until the value null
+        if(fgets(raw_product[line],100, file_product) != NULL)
+        {
+            // count length of the line in the file
+            line += 1;
+        }
+    }
+    
+    // closing file
+    fclose(file_product);
+    
+    char data[100][100][100];
+    int len_data;
+    
+    // splitting , 
+    for(int i = 0; i <2;i++)
+    {
+        // Returns first token 
+        char *token = strtok(raw_product[i], ",");
+        
+        // Keep printing tokens while one of the
+        // delimiters present in str[].
+        while (token != NULL)
+        {
+            for(int j = 0; j < 5; j++)
+            {
+                // copy the splitted string adn insert i into data
+                strncpy(data[i][j], token, 99);
+                data[i][j][99] = '\0'; 
+                token = strtok(NULL, ",");
+            }
+        }
+        
+    }
+    
+    char product_id[100][100];
+    // insert the data into product id
+    for(int i=0; i < line ;i++)
+    {
+        strncpy(product_id[i],data[i][0],99); 
+    }
+            
+    while(condition == 'y' || condition == 'Y')
+    {
+
+        // getting date input
+        while(1)
+        {
+            int array_len = 0;
+            
+            // getting user input
+            printf("Enter the date (dd-mm-yyyy): ");
+            scanf("%s", &record.date);
+            
+            // getting arr len
+            arr_len(record.date, &array_len);
+            
+            // further date validation
+            if(array_len == 10)
+            {
+                if(record.date[2] == '-' && record.date[5] == '-')
+                {
+                    // ensure the date not above 31
+                    if( (int) (((record.date[0] - '0') * 10) + record.date[1] - '0') < 31) // subtract with '0' will return the int value (converting ascii to int)
+                    {
+                        // ensure the month not above 12
+                        if((int) (((record.date[3] - '0') * 10) + record.date[4] - '0') < 12)
+                        {
+                            while ((ch = getchar()) != '\n' && ch != EOF);// clearing previous input     
+                            break;
+                        }
+                        
+                        else
+                        {
+                            printf("The monnth is more than 31, pls stry again\n");
+                            while ((ch = getchar()) != '\n' && ch != EOF);// clearing previous input     
+                        }
+                    }
+                    
                     else
                     {
-                        printf("The monnth is more than 31, pls stry again\n");
+                        printf("The date is more than 31, pls stry again\n");
                         while ((ch = getchar()) != '\n' && ch != EOF);// clearing previous input     
                     }
+                
                 }
-    
+                
                 else
                 {
-                    printf("The date is more than 31, pls stry again\n");
-                    while ((ch = getchar()) != '\n' && ch != EOF);// clearing previous input     
+                    printf("pls follow the data format put the '-' between the date\n");
+                    while ((ch = getchar()) != '\n' && ch != EOF);// clearing previous input
                 }
                 
             }
-
-            else
+            
+            else if(array_len < 10)
             {
-                printf("pls follow the data format put the '-' between the date\n");
+                printf("Pls input the right format\n");
                 while ((ch = getchar()) != '\n' && ch != EOF);// clearing previous input
             }
             
-        }
-
-        else if(array_len < 10)
-        {
-            printf("Pls input the right format\n");
-            while ((ch = getchar()) != '\n' && ch != EOF);// clearing previous input
-        }
-        
-        else
-        {
+            else
+            {
             printf("Pls input the right format\n");
             while ((ch = getchar()) != '\n' && ch != EOF);// clearing previous input
         }   
         
     }
     
-    // getting product id input
-    while(1)
+        // getting product id input
+        while(1)
     {
         int array_len = 0;
         
@@ -184,83 +247,52 @@ int add_record()
         
         // getting arr len for valdiation
         arr_len(record.product_id, &array_len);
-
-        // open the product.txt
-        file_product = fopen(product_location,"r");
-
-        if( file_product == NULL)
+        
+        if(array_len == 4)
         {
-            printf("Error opening file.\n");
-            return 1;
-        }
-
-        int line = 0;
-        char raw_product [100] [100];
-
-        // take all line to the array until not end of file and not error
-        while(!feof(file_product) && !ferror(file_product))
-        {
-            // looping until the value null
-            if(fgets(raw_product[line],100, file_product) != NULL)
+            
+            // checking the product id inside the product.txt
+            char *condition = "False";
+            for(int i = 0; i < line; i++)
             {
-                // get the length of the line
-                line += 1;
-            }
-        }
-
-        // closing file
-        fclose(file_product);
-
-        char data[100][100][100];
-        int len_data;
-
-        // splitting , 
-        for(int i = 0; i <2;i++)
-        {
-            // Returns first token 
-            char *token = strtok(raw_product[i], ",");
-
-            // Keep printing tokens while one of the
-            // delimiters present in str[].
-            while (token != NULL)
-            {
-                for(int j = 0; j < 5; j++)
+                // return the true when it same
+                if(strcmp(record.product_id, product_id[i]) == 0)
                 {
-                    // copy the splitted string adn insert i into data
-                    strncpy(data[i][j], token, 99);
-                    data[i][j][99] = '\0'; 
-                    token = strtok(NULL, ",");
+                    condition = "True";
+                    while ((ch = getchar()) != '\n' && ch != EOF);// clearing previous input
+                    break;
+                }
+                
+                // give error message when the product id not registered
+                else if(i == line - 1 && strcmp(record.product_id, product_id[i]) != 0)
+                {
+                    condition = "False";
+                    printf("The product haven't addded yet, the product id not in the product.txt\n");
                 }
             }
-    
+            
+            // break if the condition is true
+            if(condition == "True")
+            {
+                break;
+            }
+            
         }
-
-        char product_id[100][100];
-
-        // insert the data into product id
-        for(int i=0; i < line ;i++)
-        {
-            strncpy(product_id[i],data[i][0],99); 
-        }
-
-
-
-        if(array_len == 5)
+        
+        else
         {
             while ((ch = getchar()) != '\n' && ch != EOF);// clearing previous input
-            break;
-        }
-
-        {
-            while ((ch = getchar()) != '\n' && ch != EOF);// clearing previous input
-            printf("Pls enter the right format");
+            printf("Pls enter the right format\n");
         }
     }
+    
+        // getting the quantity
+        integer_valdiation("Enter the quantity: ", &record.quantity);
 
-    // // getting the quantity
-    // integer_valdiation("Enter the quantity: ", &record.quantity);
-
-    // getting status
+        printf("Do you still want to contiinue (y/n): ");
+        scanf("%c", &condition);
+    }
+    
 }
 
 int main()
